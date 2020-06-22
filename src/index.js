622c823c21f29b3aef36ff45232add33138a53fe
Matthew Gleich <matthewgleich@gmail.com>
2020-06-21 22:57:59 -0400

    ✨ Update index.js
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a react component
-const App = function () {
-  return <div>Hi there!</div>;
+const App = () => {
+  const buttonText = 'Click on me!';
+  const labelText = 'Enter name:';
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {buttonText}
+      </button>
+    </div>
+  );
 };
 
 // Take a react component and show it on the screen
